bb189ef0aac2b015f6399a60457943c87b3732c7
aliGeeee <zhepu.gu@gmail.com>
2018-11-27 15:22:32 +1100

    fixed login #2

--- PH_backend/PHapp/urls.py
+++ PH_backend/PHapp/urls.py
@@ -5,6 +5,4 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^puzzles/$', views.puzzles, name='puzzles'),
-	url(r'^login/$', auth_views.login, name='login'),
-	url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
 ]
