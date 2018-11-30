3b41a07530ba9efa61d52449e51ea8ced4ae126f
aliGeeee <zhepu.gu@gmail.com>
2018-11-30 13:41:09 +1100

    serves static 2

--- PH_backend/PH_backend/settings.py
+++ PH_backend/PH_backend/settings.py
@@ -1,5 +1,6 @@
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,6 +77,7 @@ DATABASES = {
         'HOST': 'ec2-50-19-249-121.compute-1.amazonaws.com',
         'PORT': '5432',
     }
+    # 'default':dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 LOGIN_URL = '/login/'
@@ -117,10 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-CSRF_USE_SESSIONS = True
-
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+STATIC_URL = '/puzzle/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    'PHapp/puzzleFiles'
+]
+
+CSRF_USE_SESSIONS = True
--- PH_backend/PHapp/urls.py
+++ PH_backend/PHapp/urls.py
@@ -5,11 +5,11 @@ from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
+settings.STATIC_URL = '/puzzle/'
+
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	#url(r'^puzzle/$', views.puzzle, name='puzzles'),
 	url(r'^puzzles/$', views.puzzles, name='puzzles'),
-	path('puzzles/<path:path>/', views.specificPuzzle, name='specific puzzle'),
 	url(r'^teams/$', views.teams, name='teams'),
 	url(r'^faq/$', views.faq, name='faq'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
