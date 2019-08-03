0368ca1358d2fa244dd46b2456029b6f9c89db6e
aliGeeee <zhepu.gu@gmail.com>
2019-08-03 18:59:50 +1000

    added announcements and migrations

--- PH_backend/PHapp/admin.py
+++ PH_backend/PHapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Puzzles, Teams, SubmittedGuesses, Individuals, AltAnswers
+from .models import Puzzles, Teams, SubmittedGuesses, Individuals, AltAnswers, Announcements
 
 # Register your models here.
 
@@ -8,4 +8,4 @@ admin.site.register(Teams)
 admin.site.register(SubmittedGuesses)
 admin.site.register(Individuals)
 admin.site.register(AltAnswers)
-
+admin.site.register(Announcements)
